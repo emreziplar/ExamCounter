5143fc1095e207b56b759b8379065875d9ca88ea
Emre Zıplar <emreziplar1923@hotmail.com>
2019-08-26 18:39:32 +0300

    RemainingDaysFrame.java Edited

--- src/RemainingDaysFrame.java
+++ src/RemainingDaysFrame.java
@@ -1,32 +1,112 @@
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
+
 public class RemainingDaysFrame extends javax.swing.JFrame {
 
+    DatabaseOperations dbOperations = new DatabaseOperations();    
+    DefaultTableModel model;
+    LocalDate date = LocalDate.now();
     
     public RemainingDaysFrame() {
         initComponents();
+        model = (DefaultTableModel) informationsTable.getModel();
+        calculateDayDiff();
     }
 
+    private void calculateDayDiff()
+    {
+        ArrayList<Data> dataList = new ArrayList<>();
+        
+        dataList = dbOperations.getData();
+        
+        if(dataList != null)
+            for(Data c:dataList) 
+            {
+                LocalDate d1 = LocalDate.parse(String.valueOf(date));
+                LocalDate d2 = LocalDate.parse(c.getExamDate());
+                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
+                long dayDiff = diff.toDays();
+                
+                if(dayDiff > 0) {
+                Object[] rowData = {c.getExamName(),c.getExamDate(),dayDiff+" Day"};
+                model.addRow(rowData);        
+                }
+                else 
+                    dbOperations.deleteData(c.getExamDate());
+            }
+    }
+    
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        mainPanel = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        informationsTable = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        informationsTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Exam Name", "Exam Date", "Remaining Day"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(informationsTable);
+        if (informationsTable.getColumnModel().getColumnCount() > 0) {
+            informationsTable.getColumnModel().getColumn(0).setResizable(false);
+            informationsTable.getColumnModel().getColumn(1).setResizable(false);
+            informationsTable.getColumnModel().getColumn(2).setResizable(false);
+            informationsTable.getColumnModel().getColumn(2).setPreferredWidth(10);
+        }
+
+        javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+        mainPanel.setLayout(mainPanelLayout);
+        mainPanelLayout.setHorizontalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGap(57, 57, 57)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(64, Short.MAX_VALUE))
+        );
+        mainPanelLayout.setVerticalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
+                .addContainerGap(222, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(79, 79, 79))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    
+    
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -61,5 +141,8 @@ public class RemainingDaysFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable informationsTable;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel mainPanel;
     // End of variables declaration//GEN-END:variables
 }

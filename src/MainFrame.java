b8d7de5c996715a0132331fdd23e83344d8cd73e
Emre Zıplar <emreziplar1923@hotmail.com>
2019-08-26 18:38:49 +0300

    MainFrame.java Edited

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,20 +1,10 @@
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import javax.swing.table.DefaultTableModel;
-
-
 public class MainFrame extends javax.swing.JFrame {
 
     DatabaseOperations dbOperations = new DatabaseOperations();
-    LocalDate date = LocalDate.now();
-    DefaultTableModel model;
      
     public MainFrame() {
         initComponents();
-        model = (DefaultTableModel) tempTable.getModel();
     }
 
     
@@ -27,8 +17,6 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         seeButton = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        tempTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -65,32 +53,6 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 0, 0));
         jLabel2.setText("See Remaining Times");
 
-        tempTable.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-
-            },
-            new String [] {
-                "Exam Name", "Exam Date", "Remaining Day"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                false, false, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        jScrollPane2.setViewportView(tempTable);
-        if (tempTable.getColumnModel().getColumnCount() > 0) {
-            tempTable.getColumnModel().getColumn(0).setResizable(false);
-            tempTable.getColumnModel().getColumn(0).setPreferredWidth(10);
-            tempTable.getColumnModel().getColumn(1).setResizable(false);
-            tempTable.getColumnModel().getColumn(1).setPreferredWidth(10);
-            tempTable.getColumnModel().getColumn(2).setResizable(false);
-            tempTable.getColumnModel().getColumn(2).setPreferredWidth(20);
-        }
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -100,18 +62,16 @@ public class MainFrame extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(134, 134, 134))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(166, 166, 166)
-                            .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(164, 164, 164)
-                            .addComponent(seeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(53, 53, 53)
-                            .addComponent(jLabel2))))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(166, 166, 166)
+                        .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(164, 164, 164)
+                        .addComponent(seeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(53, 53, 53)
+                        .addComponent(jLabel2)))
                 .addContainerGap(75, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -125,9 +85,7 @@ public class MainFrame extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(seeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(20, Short.MAX_VALUE))
+                .addContainerGap(123, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -146,36 +104,19 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
         dbOperations.createTable();
-        dbOperations.addData("Fizik", "2019-08-27");
-        dbOperations.addData("Matematik", "2020-08-29");
-        System.out.println(date.getDayOfYear());
+        
+        setVisible(false);
+        AddExamFrame addExamFrame = new AddExamFrame();
+        addExamFrame.setVisible(true);
     }//GEN-LAST:event_addButtonActionPerformed
 
     private void seeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seeButtonActionPerformed
-        ArrayList<Data> dataList = new ArrayList<>();
-        
-        dataList = dbOperations.getData();
-        String nowDate = String.valueOf(date);
-        if(dataList != null)
-            for(Data c:dataList) 
-            {
-                LocalDate d1 = LocalDate.parse(String.valueOf(date), DateTimeFormatter.ISO_LOCAL_DATE);
-                LocalDate d2 = LocalDate.parse(c.getExamDate(), DateTimeFormatter.ISO_LOCAL_DATE);
-                Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
-                long diffDays = diff.toDays();
-                
-                Object[] rowData = {c.getExamName(),c.getExamDate(),diffDays+" Day"};
-                model.addRow(rowData);
-                
-                
-                
-                
-            }
+        setVisible(false);
+        RemainingDaysFrame remainingDaysFrame = new RemainingDaysFrame();
+        remainingDaysFrame.setVisible(true);
     }//GEN-LAST:event_seeButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String args[]) {
         
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -191,8 +132,6 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton seeButton;
-    private javax.swing.JTable tempTable;
     // End of variables declaration//GEN-END:variables
 }
